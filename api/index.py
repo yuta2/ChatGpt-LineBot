dddd7cefea14360600687accfe0ddd8816c189cd
Yuta Tu <yuta_tu@hotmail.com>
2023-02-16 12:41:51 +0800

    list_rich_menu

--- api/index.py
+++ api/index.py
@@ -57,7 +57,8 @@ def handle_message(event):
     if event.message.type != "list_rich_menu":
         rich_menu_list = line_bot_api.get_rich_menu_list()
         for rich_menu in rich_menu_list:
-            print(rich_menu.rich_menu_id)
+            # print(rich_menu.rich_menu_id)
+            reply_msg = reply_msg+','+rich_menu.rich_menu_id
         return
 
     if working_status:
