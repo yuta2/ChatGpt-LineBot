1126089523b29d665ccf960ce9a2ca7ed0db91f9
Yuta Tu <yuta_tu@hotmail.com>
2023-03-02 14:09:32 +0800

    Changed model to gpt-3.5-turbo (ChatGPT API)

--- api/chatgpt.py
+++ api/chatgpt.py
@@ -17,17 +17,33 @@ class ChatGPT:
         self.max_tokens = int(os.getenv("OPENAI_MAX_TOKENS", default = 4096))
 
     def get_response(self):
+        # response = openai.ChatCompletion.create(
+        #     model=self.model,
+        #     messages=[{"role": "user", "content":"HI"}],
+        #     temperature=self.temperature,
+        #     # frequency_penalty=self.frequency_penalty,
+        #     # presence_penalty=self.presence_penalty,
+        #     stop=None,
+        #     max_tokens=self.max_tokens
+        # )
+        # return response
+        # # return response['choices'][0]['text'].strip()
+        # Use OpenAI's ChatCompletion API to get the chatbot's response
         response = openai.ChatCompletion.create(
-            model=self.model,
-            messages=[{"role": "user", "content":"HI"}],
-            temperature=self.temperature,
-            # frequency_penalty=self.frequency_penalty,
-            # presence_penalty=self.presence_penalty,
-            stop=None,
-            max_tokens=self.max_tokens
+            model="gpt-3.5-turbo",  # The name of the OpenAI chatbot model to use
+            messages=message_log,  # The conversation history up to this point, as a list of dictionaries
+            max_tokens=4096,  # The maximum number of tokens (words or subwords) in the generated response
+            stop=None,  # The stopping sequence for the generated response, if any (not used here)
+            temperature=0.7,  # The "creativity" of the generated response (higher temperature = more creative)
         )
-        return response
-        # return response['choices'][0]['text'].strip()
+
+        # Find the first response from the chatbot that has text in it (some responses may not have text)
+        for choice in response.choices:
+            if "text" in choice:
+                return choice.text
+
+        # If no response with text is found, return the first response's content (which may be empty)
+        return response.choices[0].message.content
 
     def add_msg(self, text):
         self.prompt.add_msg(text)
