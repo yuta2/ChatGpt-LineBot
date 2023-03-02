4c7a2d6a7483c3a636836618a0f2af1ea4dafea5
Yuta Tu <yuta_tu@hotmail.com>
2023-03-02 14:38:22 +0800

    Changed model to gpt-3.5-turbo (ChatGPT API)

--- api/chatgpt.py
+++ api/chatgpt.py
@@ -12,19 +12,20 @@ class ChatGPT:
         self.temperature = float(os.getenv("OPENAI_TEMPERATURE", default = 0.7))
         # self.frequency_penalty = float(os.getenv("OPENAI_FREQUENCY_PENALTY", default = 0))
         # self.presence_penalty = float(os.getenv("OPENAI_PRESENCE_PENALTY", default = 0.6))
-        self.max_tokens = int(os.getenv("OPENAI_MAX_TOKENS", default = 4096))
+        self.max_tokens = int(os.getenv("OPENAI_MAX_TOKENS", default = 3000))
 
     def get_response(self):
         # Use OpenAI's ChatCompletion API to get the chatbot's response
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",   # The name of the OpenAI chatbot model to use
-            messages=[{"role": "user", "content": self.prompt.generate_prompt().strip()}],
+            messages=[{"role": "user", "content": self.prompt.generate_prompt()}],
             temperature=self.temperature,   # The "creativity" of the generated response (higher temperature = more creative)
             stop=None,                      # The stopping sequence for the generated response, if any (not used here)
             max_tokens=self.max_tokens      # The maximum number of tokens (words or subwords) in the generated response
         )
 
-        return response.choices[0].message.content
+        return self.prompt.generate_prompt()
+        # return response.choices[0].message.content
 
     def add_msg(self, text):
         self.prompt.add_msg(text)
