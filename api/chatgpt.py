74d94870d28ef9d25a523fa5638114ce074d4d16
Yuta Tu <yuta_tu@hotmail.com>
2023-03-02 14:16:39 +0800

    Changed model to gpt-3.5-turbo (ChatGPT API)

--- api/chatgpt.py
+++ api/chatgpt.py
@@ -29,13 +29,13 @@ class ChatGPT:
         # return response
         # # return response['choices'][0]['text'].strip()
         # Use OpenAI's ChatCompletion API to get the chatbot's response
-        response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",  # The name of the OpenAI chatbot model to use
-            messages=[{"role": "user", "content": self.prompt.generate_prompt()}],  # The conversation history up to this point, as a list of dictionaries
-            max_tokens=4096,  # The maximum number of tokens (words or subwords) in the generated response
-            stop=None,  # The stopping sequence for the generated response, if any (not used here)
-            temperature=0.7,  # The "creativity" of the generated response (higher temperature = more creative)
-        )
+        # response = openai.ChatCompletion.create(
+        #     model="gpt-3.5-turbo",  # The name of the OpenAI chatbot model to use
+        #     messages=[{"role": "user", "content": self.prompt.generate_prompt()}],  # The conversation history up to this point, as a list of dictionaries
+        #     max_tokens=4096,  # The maximum number of tokens (words or subwords) in the generated response
+        #     stop=None,  # The stopping sequence for the generated response, if any (not used here)
+        #     temperature=0.7,  # The "creativity" of the generated response (higher temperature = more creative)
+        # )
 
         # Find the first response from the chatbot that has text in it (some responses may not have text)
         # for choice in response.choices:
@@ -43,7 +43,13 @@ class ChatGPT:
         #         return choice.text
 
         # If no response with text is found, return the first response's content (which may be empty)
-        # return response.choices[0].message.content
-        return self.prompt.generate_prompt()
+
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[{"role": "user", "content": "Tell the world about the ChatGPT API in the style of a pirate."}]
+        )
+
+        return response.choices[0].message.content
+        # return self.prompt.generate_prompt()
     def add_msg(self, text):
         self.prompt.add_msg(text)
