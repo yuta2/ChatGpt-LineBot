e70d8da3451ee328cff1969ac1baaa8d911f0a84
Yuta Tu <yuta_tu@hotmail.com>
2023-03-02 14:28:49 +0800

    Changed model to gpt-3.5-turbo (ChatGPT API)

--- api/chatgpt.py
+++ api/chatgpt.py
@@ -17,7 +17,7 @@ class ChatGPT:
     def get_response(self):
         # Use OpenAI's ChatCompletion API to get the chatbot's response
         response = openai.ChatCompletion.create(
-            model=self.model,   # The name of the OpenAI chatbot model to use
+            model="gpt-3.5-turbo",   # The name of the OpenAI chatbot model to use
             messages=[{"role": "user", "content": self.prompt.generate_prompt()}],
             temperature=self.temperature,   # The "creativity" of the generated response (higher temperature = more creative)
             stop=None,                      # The stopping sequence for the generated response, if any (not used here)
